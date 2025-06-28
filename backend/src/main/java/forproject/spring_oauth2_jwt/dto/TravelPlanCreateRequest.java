b08a6789a09621298dcb5598a3cb05d1b160260c
Losiento_nana <edcqazs123@naver.com>
2025-06-28 16:03:46 +0900

    fix DTO

---
+++ backend/src/main/java/forproject/spring_oauth2_jwt/dto/TravelPlanCreateRequest.java
@@ -0,0 +1,10 @@
+//package forproject.spring_oauth2_jwt.dto;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//public class TravelPlanCreateRequest {
+//
+//}
---
+++ backend/src/main/java/forproject/spring_oauth2_jwt/repository/TravelPlanRepository.java
@@ -0,0 +1,10 @@
+package forproject.spring_oauth2_jwt.repository;
+
+import forproject.spring_oauth2_jwt.entity.TravelPlanEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TravelPlanRepository extends JpaRepository<TravelPlanRepository,Long> {
+    List<TravelPlanEntity> findByUser_Id(Long userId); // 유저별 일정 조회
+}
